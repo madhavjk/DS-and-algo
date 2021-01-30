86c3fd6dfa7af913242a76f277c33fc519a8260f
Madhav Sharma <42112240+madhavjk@users.noreply.github.com>
2021-01-30 23:29:05 +0530

    Add files via upload

---
+++ Mathematics/GCD.cpp
@@ -0,0 +1,71 @@
+
+#Euclidean Algo code
+
+
+
+
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+ int gcd(int a, int b)
+{
+	while(a!=b)
+	{
+		if(a > b)
+			a = a - b;
+		else
+			b = b - a; 
+	}
+
+	return a;
+}
+
+int main() {
+    
+    	int a = 12, b = 15;
+    	
+    	cout<<gcd(a, b);
+    	
+    	return 0;
+}
+
+
+
+
+
+
+
+
+# Optimised Euclidean Algorithm Code
+
+
+
+
+
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+ int gcd(int a, int b)        
+{
+	if(b==0)
+		return a;
+
+	return gcd(b, a % b);
+}
+
+int main() {
+    
+    	int a = 12, b = 15;
+    	
+    	cout<<gcd(a, b);
+    	
+    	return 0;
+}
+
+
+
+#Output
+
+3
